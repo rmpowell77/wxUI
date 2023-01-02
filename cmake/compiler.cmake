afe36337794f18475cea9a4cf7bbc935b4ddf67f
Richard Powell <rmpowell77@me.com>
2023-01-02 07:25:09 -0800

    Issue #47: cmake macros should have a namespace

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,12 +1,11 @@
 # compiler.cmake
 # any compiler specific details do them here
 
-macro(SetupCompilerForTarget arg)
+macro(wxUI_SetupCompilerForTarget arg)
   if(NOT MSVC)
   target_compile_options(${arg} PRIVATE -Wall -Wextra)
   endif()
   set_target_properties(${arg} PROPERTIES CXX_STANDARD 20)
-  set_target_properties(${arg} PROPERTIES CMAKE_COMPILE_WARNING_AS_ERROR ON)
   set_property(TARGET ${arg} PROPERTY COMPILE_WARNING_AS_ERROR ON)
 endmacro()
 
