33b489436f8a07cefd96511946d3721ecfd4998e
Richard Powell <rmpowell77@me.com>
2025-11-23 10:05:57 -0800

    Issue #235: Increment the minimum support for wxUI to be c++23

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -5,7 +5,7 @@ macro(wxUI_SetupCompilerForTarget arg)
   if(NOT MSVC)
   target_compile_options(${arg} PRIVATE -Wall -Wextra)
   endif()
-  set_target_properties(${arg} PROPERTIES CXX_STANDARD 20)
+  set_target_properties(${arg} PROPERTIES CXX_STANDARD 23)
   set_property(TARGET ${arg} PROPERTY COMPILE_WARNING_AS_ERROR ON)
 endmacro()
 
