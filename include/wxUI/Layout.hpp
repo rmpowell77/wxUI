68f9044c5c30006a2ff12aa97689efd80d8b45ae
owacoder <owacoder@gmail.com>
2026-01-29 17:46:33 -0500

    Added missing cast

--- include/wxUI/Layout.hpp
+++ include/wxUI/Layout.hpp
@@ -397,7 +397,7 @@ private:
         auto* book = constructBook(parent);
 
         std::apply([book, flags](auto&&... tupleArg) {
-            (details::createAndAddVisiter(tupleArg, book, nullptr, flags), ...);
+            (details::createAndAddVisiter(tupleArg, book, static_cast<wxBoxSizer*>(nullptr), flags), ...);
         },
             items_);
 
