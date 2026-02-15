516b2f92b8afe63bb8f74fd8d45c3742d58b09c3
Richard Powell <rmpowell77@me.com>
2026-02-15 19:01:01 +0100

    Issue #264: Break LayoutIf into it's own header (#265)

---
+++ tests/BuildTests/LayoutIf.cpp
@@ -0,0 +1,50 @@
+/*
+MIT License
+
+Copyright (c) 2022-2026 Richard Powell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+// Generated with bash command
+// for i in *.cpp; do echo \#include \<wxUI\/Layout.hpp\> > Layout.cpp; for j in WXUI_RULE_OF_SIX_BOILERPLATE WXUI_WIDGET_STATIC_ASSERT_BOILERPLATE; do echo \#if defined\(WXUI_WIDGET_STATIC_ASSERT_BOILERPLATE\) >> Layout.cpp; echo \#error Missed zapping WXUI_WIDGET_STATIC_ASSERT_BOILERPLATE >> Layout.cpp; echo \#endif >> Layout.cpp; done; done
+#include <wxUI/LayoutIf.hpp>
+
+#if defined(WXUI_RULE_OF_SIX_BOILERPLATE)
+#error Missed zapping WXUI_RULE_OF_SIX_BOILERPLATE
+#endif
+
+#if defined(WXUI_WIDGET_STATIC_ASSERT_BOILERPLATE)
+#error Missed zapping WXUI_WIDGET_STATIC_ASSERT_BOILERPLATE
+#endif
+
+#if defined(WXUI_WIDGET_CREATE_BOILERPLATE)
+#error Missed zapping WXUI_WIDGET_CREATE_BOILERPLATE
+#endif
+
+#if defined(WXUI_FORWARD_TO_DETAILS)
+#error Missed zapping WXUI_FORWARD_TO_DETAILS
+#endif
+
+#if defined(WXUI_FORWARD_BIND_TO_DETAILS)
+#error Missed zapping WXUI_FORWARD_BIND_TO_DETAILS
+#endif
+
+#if defined(WXUI_FORWARD_ALL_TO_DETAILS)
+#error Missed zapping WXUI_FORWARD_ALL_TO_DETAILS
+#endif
